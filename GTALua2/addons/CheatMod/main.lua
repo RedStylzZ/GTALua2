345fe04798b07d8a9bb4cb3679b055df9817868d
MockbaTheBorg <mockba.the.borg@gmail.com>
2017-03-08 20:23:05 -0500

    Added some stats research info

--- GTALua2/addons/CheatMod/main.lua
+++ GTALua2/addons/CheatMod/main.lua
@@ -91,6 +91,22 @@ function CheatMod:Run()
 	end
 end
 
+function StatGetString(statName)
+	return natives.STATS.STAT_GET_STRING(joaat(statName), -1)
+end
+
+function StatGetDate(statName)
+	local value = Cmem:new(8)
+	natives.STATS.STAT_GET_DATE(joaat(statName), value, 7, -1)
+	return value:getInt(0)
+end
+
+function StatGetInt(statName)
+	local value = Cvar:new()
+	natives.STATS.STAT_GET_INT(joaat(statName), value, -1)
+	return value:getInt()
+end
+
 function CheatMod:Process()
 
 	ui.ShowHudComponent(HudComponentReticle)	
@@ -340,6 +356,47 @@ function CheatMod:Process()
 			end
 		end
 	end
+
+-- List Stats info
+	if IsKeyJustDown(KEY_F5) then
+		print("---------------------------")
+		for k, name in pairs({
+			"mp0_char_name",
+			"mp0_biggest_victim_name",
+			"mp0_archenemy_name",
+			"mp0_gb_office_name" }) do
+			print(name, StatGetString(name))
+		end
+		for k, name in pairs({
+			"mpply_repeat_offender_1_time",
+			"mpply_repeat_offender_2_time" }) do
+			print(name, StatGetDate(name))
+		end
+		for k, name in pairs({
+			"mp0_total_playing_time",
+			"mp0_dist_driving_car",
+			"mp0_dist_driving_plane",
+			"mp0_dist_driving_heli",
+			"mp0_dist_driving_bike",
+			"mp0_longest_drive_nocrash",
+			"mp0_pistol_in_possession",
+			"mp0_mg_in_possession",
+			"mpply_seasontoclear",
+			"mpply_currentvehsalesseason",
+			"mpply_currentvehsalestuseason",
+			"mpply_num_cars_sold_today",
+			"mpply_char_exploit_level",
+			"mpply_char_peak_exploit_level",
+			"mpply_last_time_thresh_cross",
+			"mpply_became_cheater_num",
+			"mpply_prevseasonexploitlevel",
+			"mpply_prevseason2exploitlevel",
+			"mpply_prevseason3exploitlevel",
+			"mpply_prevseason4exploitlevel" }) do
+			print(name, StatGetInt(name))
+		end
+	end
+
 -- Teleport few meters ahead
 	if IsKeyJustDown(KEY_E) then
 		local plr = LocalPlayer()
